6180289b57e3a6e7999100ebf8fe65117d152f27
Samuel Park <sampark1080@gmail.com>
2024-09-29 23:34:46 -0700

    Lab3 update

--- modules/utils.js
+++ modules/utils.js
@@ -1,9 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
 class Utils {
-    static getDate() {
+    getDate() {
         return new Date().toString();
     }
+
+    getGreetingMessage(name, greetingTemplate) {
+        const currentTime = this.getDate();
+        return greetingTemplate.replace('%1', name).replace('%2', currentTime);
+    }
+
+    writeToFile(text, callback) {
+      const filePath = path.join(__dirname, '../file.txt');
+
+      fs.writeFile(filePath, text + '\n', { flag: 'a' }, (err) => {
+          callback(err);
+      });
+  }
+
+  readFromFile(filePath, callback) {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            callback(err, null);
+        } else {
+            callback(null, data);
+        }
+    });
 }
-
-
+}
 
 module.exports = Utils;
--- server.js
+++ server.js
@@ -1,93 +1,74 @@
-// This file used ChatGPT to help code
 const http = require('http');
 const url = require('url');
+const Utils = require('./modules/Utils');
 const fs = require('fs');
-const path = require('path');
-const Utils = require('./modules/utils');
-const Messages = require('./lang/en/en');
+
+const messages = JSON.parse(fs.readFileSync('./lang/en/en.json', 'utf-8'));
 
 class Server {
-    static start() {
-        const server = http.createServer(this.handleRequest.bind(this));
-
-        const port = process.env.PORT || 3000;
-        server.listen(port, () => {
-            console.log(`Server is running on port ${port}`);
-        })
+    constructor() {
+        this.utils = new Utils();
     }
 
-    static handleRequest(req, res) {
-        const parsedUrl = url.parse(req.url, true);
-        const pathname = parsedUrl.pathname;
-        const query = parsedUrl.query;
+    start() {
+        const server = http.createServer((req, res) => {
+            const parsedUrl = url.parse(req.url, true);
+            const path = parsedUrl.pathname;
+            const query = parsedUrl.query;
 
-        if (pathname.includes('/writeFile')) {
-            this.writeFile(query.text, res);
-        } else if (pathname.includes('/readFile')) {
-            const filePath = path.join(__dirname, 'file.txt');
-            this.readFile(filePath, res);
-        } else {
-            this.greetUser(query, res);  // Serve the greet user page by default
-        }
-    }
-
-    // C.1: Append text to a file
-    static writeFile(text, res) {
-        const filePath = path.join(__dirname, 'file.txt');
-        if (!text) {
-            res.writeHead(400, { 'Content-Type': 'text/plain' });
-            res.end('Error: No text provided in query.');
-            return;
-        }
-
-        // Append text to the file (create if it doesn't exist)
-        fs.appendFile(filePath, text + '\n', (err) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end('Error writing to file');
-                return;
-            }
-
-            res.writeHead(200, { 'Content-Type': 'text/plain' });
-            res.end(`Text "${text}" appended to file.txt`);
-        });
-    }
-
-    // C.2: Read the file and return its content
-    static readFile(filePath, res) {
-        // Check if the file exists
-        fs.access(filePath, fs.constants.F_OK, (err) => {
-            if (err) {
-                res.writeHead(404, { 'Content-Type': 'text/plain' });
-                res.end(`Error 404: ${path.basename(filePath)} not found`);
-                return;
-            }
-
-            // Read the file content
-            fs.readFile(filePath, 'utf8', (err, data) => {
-                if (err) {
-                    res.writeHead(500, { 'Content-Type': 'text/plain' });
-                    res.end('Error reading the file');
-                    return;
+            if (path === '/COMP4537/labs/3/getDate/') {
+                const name = query.name;
+                if (name) {
+                    const message = this.utils.getGreetingMessage(name, messages.greeting);
+                    res.writeHead(200, { 'Content-Type': 'text/html' });
+                    res.end(`<p style="color:blue">${message}</p>`);
+                } else {
+                    res.writeHead(400, { 'Content-Type': 'text/html' });
+                    res.end(`<p style="color:red">${messages.missingName}</p>`);
                 }
 
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end(data);
-            });
+            } else if (path === '/COMP4537/labs/3/writeFile/') {
+                const text = query.text;
+                if (text) {
+                    this.utils.writeToFile(text, (err) => {
+                        if (err) {
+                            res.writeHead(500, { 'Content-Type': 'text/html' });
+                            res.end(`<p style="color:red">${messages.writeError}</p>`);
+                        } else {
+                            res.writeHead(200, { 'Content-Type': 'text/html' });
+                            res.end(`<p style="color:green">${messages.writeSuccess}</p>`);
+                        }
+                    });
+                } else {
+                    res.writeHead(400, { 'Content-Type': 'text/html' });
+                    res.end(`<p style="color:red">${messages.missingText}</p>`);
+                }
+
+            } else if (path.startsWith('/COMP4537/labs/3/readFile/')) {
+              const requestedFile = path.split('/').pop(); 
+              const filePath = `./${requestedFile}`; 
+
+              this.utils.readFromFile(filePath, (err, data) => {
+                  if (err) {
+                      const errorMessage = messages.fileNotFound.replace('%1', requestedFile);
+                      res.writeHead(404, { 'Content-Type': 'text/html' });
+                      res.end(`<p style="color:red">${errorMessage}</p>`);
+                  } else {
+                      res.writeHead(200, { 'Content-Type': 'text/html' });
+                      res.end(`<pre>${data}</pre>`);
+                  }
+              });  
+            } else {
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.end(`<p style="color:red">${messages.invalidEndpoint}</p>`);
+            }
         });
-    }
 
-    // Original greetUser function from Part B
-    static greetUser(query, res) {
-        const name = query.name || 'Guest';  // Default to 'Guest' if no name is provided
-        const serverTime = Utils.getDate();
-        let message = Messages.greeting.replace('%1', name).replace('%2', serverTime);
-        const styledMessage = `<div style="color: blue;">${message}</div>`;
-
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(styledMessage);
+        server.listen(3000, () => {
+            console.log('Server running on port 3000');
+        });
     }
 }
 
-// Start the server
-Server.start();
+const myServer = new Server();
+myServer.start();
